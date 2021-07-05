11ff625418244775848d509317d5720d07ecbd10
Bojan <bojan.gavreski@students.finki.ukim.mk>
2021-07-05 18:54:03 +0200

    Image test

--- src/components/ProductForm.js
+++ src/components/ProductForm.js
@@ -1,10 +1,10 @@
-import React,{useEffect,useState} from "react";
-import AddImage from './AddImage';
-import { Form} from "semantic-ui-react";
+import React, { useEffect, useState } from "react";
+import AddImage from "./AddImage";
+import { Form } from "semantic-ui-react";
 import gql from "graphql-tag";
 import { useForm } from "../util/hooks";
-import { useMutation,useQuery } from "@apollo/react-hooks";
-import '../App.css';
+import { useMutation, useQuery } from "@apollo/react-hooks";
+import "../App.css";
 
 import {
   Container,
@@ -15,22 +15,22 @@ import {
   makeStyles,
   Select,
   MenuItem,
-  InputLabel
-} from '@material-ui/core';
-const CATEGORIES =require('../util/categories');
+  InputLabel,
+} from "@material-ui/core";
+const CATEGORIES = require("../util/categories");
 
 function ProductForm(props) {
-  const [cities,setCities] = useState([]);
-  const {loading, data} = useQuery(FETCH_CITIES);
+  const [cities, setCities] = useState([]);
+  const { loading, data } = useQuery(FETCH_CITIES);
 
   const { values, onChange, onSubmit } = useForm(createProductCallback, {
-     title: "",
-     description:"",
-     location:"",
-     image:"",
-     category:"",
-     region:"",
-     creator:props.user.id,
+    title: "",
+    description: "",
+    location: "",
+    image: "",
+    category: "",
+    region: "",
+    creator: props.user.id,
   });
 
   const username = props.user.email;
@@ -38,21 +38,21 @@ function ProductForm(props) {
   const useStyles = makeStyles((theme) => ({
     paper: {
       marginTop: theme.spacing(8),
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
     },
     avatar: {
       margin: theme.spacing(1),
-      backgroundColor: '#03941b',
+      backgroundColor: "#03941b",
     },
     form: {
-      width: '100%', // Fix IE 11 issue.
+      width: "100%", // Fix IE 11 issue.
       marginTop: theme.spacing(1),
     },
     submit: {
       margin: theme.spacing(3, 0, 2),
-      backgroundColor: '#03941b',
+      backgroundColor: "#03941b",
     },
   }));
   const classes = useStyles();
@@ -61,49 +61,54 @@ function ProductForm(props) {
     update(proxy, result) {
       const data = proxy.readQuery({
         query: FETCH_PRODUCTS_BY_USER_QUERY,
-        variables:{
-             username
-        }
+        variables: {
+          username,
+        },
       });
       //data.getPosts = [result.data.createPost, ...data.getPosts];
-        //data.getProducts = [result.data.createProduct, ...data.getProducts];
+      //data.getProducts = [result.data.createProduct, ...data.getProducts];
 
-      proxy.writeQuery({ query: FETCH_PRODUCTS_BY_USER_QUERY, data:{
-          getProductsByUser: [...data.getProductsByUser,result.data.createProduct]  
-      }});
-      values.title= "";
-      values.description="";
-      values.location="";
-      values.image="";
-      values.category="";
-      values.region="";
-      values.creator="";
+      proxy.writeQuery({
+        query: FETCH_PRODUCTS_BY_USER_QUERY,
+        data: {
+          getProductsByUser: [
+            ...data.getProductsByUser,
+            result.data.createProduct,
+          ],
+        },
+      });
+      values.title = "";
+      values.description = "";
+      values.location = "";
+      values.image = "";
+      values.category = "";
+      values.region = "";
+      values.creator = "";
     },
   });
   function createProductCallback() {
-   
-    createProduct().catch(e => {
- 
-    })
-  
+    createProduct().catch((e) => {});
   }
-      const cityHandler = (location) =>{
-          for(let i=0;i<cities.length;i++){
-              if(cities[i].name===location){
-                values.region=cities[i].regionId;
-                break;
-              }
-
-          }
+  const cityHandler = (location) => {
+    for (let i = 0; i < cities.length; i++) {
+      if (cities[i].name === location) {
+        values.region = cities[i].regionId;
+        break;
       }
-      useEffect(()=>{
-        if(data){
-          setCities(data.getCities);
-        };
-          cityHandler(values.location);
-      },[data,values.location])
-      return (
-      <Container component="main" maxWidth="xs" onSubmit={onSubmit}>
+    }
+  };
+  useEffect(() => {
+    if (data) {
+      setCities(data.getCities);
+    }
+    cityHandler(values.location);
+  }, [data, values.location]);
+
+  const setImage = (img) => {
+    values.image = img;
+  };
+  return (
+    <Container component="main" maxWidth="xs" onSubmit={onSubmit}>
       <CssBaseline />
       <div className={classes.paper}>
         <Typography component="h1" variant="h5">
@@ -125,67 +130,65 @@ function ProductForm(props) {
             autoFocus
           />
           <TextField
-             variant="outlined"
-             margin="normal"
-             required
-             fullWidth
-             id="description"
-             label="Опис"
-             name="description"
-             onChange={onChange}
-             value={values.description}
-             error={error ? true : false}
-             autoComplete="title"
-             autoFocus
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="description"
+            label="Опис"
+            name="description"
+            onChange={onChange}
+            value={values.description}
+            error={error ? true : false}
+            autoComplete="title"
+            autoFocus
           />
-             <TextField
-             variant="outlined"
-             margin="normal"
-             required
-             fullWidth
-             id="title"
-             label="Слика"
-             name="image"
-             onChange={onChange}
-             value={values.image}
-             error={error ? true : false}
-             autoComplete="title"
-             autoFocus
-          />
-          <AddImage/>
-         <InputLabel className=".select-label" id="demo-simple-select-helper-label">Град</InputLabel>
+          <AddImage props={setImage} />
+          <InputLabel
+            className=".select-label"
+            id="demo-simple-select-helper-label"
+          >
+            Град
+          </InputLabel>
           <Select
-          className="select-field"
-          name="location"
-          fullWidth
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          value={values.location}
-          onChange={onChange}
-          required
-        >
-          {cities ? cities.map(city =>{
-             return <MenuItem value={city.name}>{city.nameMK}</MenuItem>
-          }) 
-          :  <MenuItem>city.nameMK</MenuItem>
-          }
-        </Select>
+            className="select-field"
+            name="location"
+            fullWidth
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={values.location}
+            onChange={onChange}
+            required
+          >
+            {cities ? (
+              cities.map((city) => {
+                return <MenuItem value={city.name}>{city.nameMK}</MenuItem>;
+              })
+            ) : (
+              <MenuItem>city.nameMK</MenuItem>
+            )}
+          </Select>
 
-        <InputLabel className=".select-label" id="demo-simple-select-helper-label">Категорија</InputLabel>
+          <InputLabel
+            className=".select-label"
+            id="demo-simple-select-helper-label"
+          >
+            Категорија
+          </InputLabel>
           <Select
-          className="select-field"
-          name="category"
-          fullWidth
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          value={values.category}
-          onChange={onChange}
-          required
-        >
-          {CATEGORIES.map(category =>{
-             return <MenuItem value={category.cat}>{category.catMK}</MenuItem>
-          })  }
-        </Select>
+            className="select-field"
+            name="category"
+            fullWidth
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={values.category}
+            onChange={onChange}
+            required
+          >
+            {CATEGORIES.map((category) => {
+              return <MenuItem value={category.cat}>{category.catMK}</MenuItem>;
+            })}
+          </Select>
           <Button
             type="submit"
             fullWidth
@@ -195,10 +198,10 @@ function ProductForm(props) {
           >
             Постави
           </Button>
-          </form>
-          </div>
-          </Container>
-      );
+        </form>
+      </div>
+    </Container>
+  );
 
   // return (
   //     <>
@@ -254,12 +257,27 @@ function ProductForm(props) {
   //   )}
   //   </>
   // );
-
 }
 
 const CREATE_PRODUCT_MUTATION = gql`
-  mutation createProduct($title: String!, $description:String!, $location:String!, $image:String! $category:String! $region:String! $creator:String!) {
-    createProduct(title: $title,description:$description,location:$location,image:$image,category:$category region:$region creator:$creator) {
+  mutation createProduct(
+    $title: String!
+    $description: String!
+    $location: String!
+    $image: String!
+    $category: String!
+    $region: String!
+    $creator: String!
+  ) {
+    createProduct(
+      title: $title
+      description: $description
+      location: $location
+      image: $image
+      category: $category
+      region: $region
+      creator: $creator
+    ) {
       title
       description
       createdAt
@@ -269,20 +287,20 @@ const CREATE_PRODUCT_MUTATION = gql`
 `;
 
 const FETCH_PRODUCTS_BY_USER_QUERY = gql`
-      query($username:String!){
-        getProductsByUser(username:$username){
-            id
-            title
-        }
+  query ($username: String!) {
+    getProductsByUser(username: $username) {
+      id
+      title
     }
-    `
-  const FETCH_CITIES = gql`
-    {
-      getCities{
-        name
-        nameMK
-        regionId
-      }
+  }
+`;
+const FETCH_CITIES = gql`
+  {
+    getCities {
+      name
+      nameMK
+      regionId
     }
-  `
+  }
+`;
 export default ProductForm;
